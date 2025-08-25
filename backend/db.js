dc4d496162d4bb75c9abc3a9c54dfb536f147a72
Manisi_Nikhil_ <manisinikhil@gmail.com>
2025-08-25 21:59:36 +0530

    Update db.js
    i changed this for making it work on render

--- backend/db.js
+++ backend/db.js
@@ -2,6 +2,8 @@
 const mongoose  = require("mongoose");
 // .env
 mongoose.connect(process.env.MONGO_URI);
+console.log("MONGO_URI from env:", process.env.MONGO_URI);
+
 
 const todoSchema =  mongoose.Schema({
 
